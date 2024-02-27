3c10515a2eefe61a7287c6da0c02dff41d94cc8f
VenuriJayasinghe <venuri.20211228@iit.ac.lk>
2024-02-28 01:08:40 +0530

    Portfolio edits

---
+++ src/data/constants.js
@@ -0,0 +1,196 @@
+//this page consists of all my data
+export const Bio = {
+    name: "Venuri Jayasinghe",
+    roles: [
+      "Programmer",
+      "Conversational AI Developer",
+      "Frontend Developer",
+      "Java Developer",
+    ],
+    description:
+      "As an aspiring undergraduate actively pursuing opportunities in software engineering, I tackle challenging learning curves with enthusiasm. With a strong passion for full-stack development, I aim to apply knowledge, contribute to tech innovation, and fuel my passion. Recognized for a strong work ethic and adaptability, I am committed to making a positive impact while continuously adapting within the dynamic technology sector. ",
+    github: "https://github.com/VenuriJayasinghe",
+    resume:
+      "https://drive.google.com/drive/u/0/folders/1C3CVnqmEFRtFhRRV-F5M7dr3VDa8iuG9?fbclid=PAAaZ915pwUCGQKbWMsrO2WEzhW5c6uWkEzvb4jqy1EF_JfrVrqYTLLp8K_og",
+    linkedin: "https://www.linkedin.com/in/venuri-jayasinghe-0634ab232/ ",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+        },
+      ],
+    },
+  ];
+
+  
+  export const education = [
+    {
+      id: 0,
+      img: "http://island.lk/wp-content/uploads/2022/02/information-institute-of-technolodgy.jpg",
+      school: "Informatics Institute of Technology",
+      date: "Sep 2022 - Sep 2026",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science at Informatics Institute of Technology. I have completed the first two years. I have taken modules like Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, client-server architecture, Software development, web development and design, server-side web development. ",
+      degree: "BSc (Hons) Computer Science",
+    },
+    {
+      id: 1,
+      img: "http://island.lk/wp-content/uploads/2022/02/information-institute-of-technolodgy.jpg",
+      school: "Informatics Institute of Technology",
+      date: "Jan 2022-Sep 2022",
+      grade: "Distinction",
+      desc: "Successfully completed my foundation with a distinction pass.",
+      degree: "Foundation Certificate in Higher Education",
+
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpTQj9JlE8kYsCqyJml3x6l3kzNPgw3Z4Spg&usqp=CAU",
+      school: "Asian Grammar School",
+      date: "2010-2021",
+      grade: "Distinctions for all 9 subjects",
+      desc: "I completed my primary and secondary education at Asian Grammar School, Matara.",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 11,
+      title: "Enhanced Book Recommendation System with AI-driven Conversational Interface",
+      date: "Sep 2023 - Present",
+      description:
+        "The Book Recommendation Chatbot serves as a testament to my expertise in Natural Language Processing and my ability to leverage cutting-edge technologies to develop intelligent conversational agents that deliver personalized experiences to users.Project focus: Development of a chatbot-driven book recommendation system using NLP to tackle the cold start problem. Core development: Led development of chatbot integrating NLP techniques and OpenAI for personalized book recommendations through dynamic conversations Technology Stack : RASA, OpenAI API, MySQL, MERN ",
+      image:
+        "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+      tags: [
+        "RASA",
+        "OpenAI",
+        "LLM",
+        "MySQL",
+        "MERN",
+      ],
+      category: "web app",
+      github: "https://github.com/HarborReads",
+      webapp: "https://www.figma.com/proto/3s63CeqcakhJnXV3R8549u/SDGP-CS-22?type=design&node-id=133-155&t=Yw2PJSQxdYVl0s18-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=191%3A179",
+    },
+    {
+      id: 9,
+      title: "Westminster Shopping Management",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "Developed a comprehensive shopping system, allowing users to browse, add, and manage products in a virtual shopping environment. Implemented a modular architecture with Java programming language Utilized object-oriented principles to ensure code modularity, maintainability, and extensibility. Designed and implemented a graphical user interface (GUI) using Java Swing, providing an intuitive user. experience for product visualization and shopping cart management Technology Stack : Java, Java Swing (GUI), Object-Oriented Programming (OOP) principles, File Input/Output (I/O), Integrated Development Environment (IDE): (e.g., IntelliJ IDEA, Eclipse)",
+      tags: [
+        "Java",
+        "Java Swing (GUI)",
+        "Object-Oriented Programming (OOP)",
+      ],
+      category: "Software Development",
+      github: "https://github.com/VenuriJayasinghe",
+    },
+    {
+      id: 0,
+      title: "VollyHub Website Design",
+      date: "Jan 2023 - Mar 2023",
+      description:
+        "Designed and developed a responsive website catering to volleyball enthusiasts, providing a platform for information, resources, and community engagement.Technology Stack : HTML, CSS, JavaScript, Integrated Development Environment (IDE):Visual Studio Code",
+
+      tags: [": HTML", "CSS", "JavaScript"],
+      category: "web app",
+      github: "https://github.com/VenuriJayasinghe",
+    },
+    {
+      id: 1,
+      title: "Quiz Web-page",
+      date: "Jan 2023-march 2023",
+      description:
+        "Experience an engaging journey of knowledge exploration with our Dynamic Quiz Web Application. Crafted with HTML, CSS, and JavaScript, this responsive platform offers randomized questions, captivating animations, and seamless accessibility across all devices. ",
+      tags: [
+        "React Js",
+        "MongoDb",
+        "Node Js",
+        "Express Js",
+        "Redux",
+        "NodeMailer",
+      ],
+      category: "web app",
+      github: "https://github.com/rishavchanda/Project-Management-App",
+      webapp: "https://vexa-app.netlify.app/",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2020, text: "Started my IT journey" },
+    { year: 2021, text: "Self-learned the current trends in the IT field" },
+    { year: 2022, text: "Started my batchelors degree with IIT" },
+    { year: 2023, text: "Started my individual projects" },
+  ];
